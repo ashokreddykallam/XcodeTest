19bcd7aa7bb627c47c166c1f5db217e3ab6006b2
Ashok Reddy <kallamashokreddy23@gmail.com>
2020-07-02 14:17:13 +0530

    Initial Commit

---
+++ A2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  A2
+//
+//  Created by Ashok Reddy on 02/07/20.
+//  Copyright © 2020 Practise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
